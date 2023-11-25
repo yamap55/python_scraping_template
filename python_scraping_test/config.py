aa3447b9bbbb065a9b1336a4ce3a6a6c9f51e3e8
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-11-26 00:46:30 +0900

    feat: add config file

---
+++ python_scraping_test/config.py
@@ -0,0 +1,12 @@
+"""設定値"""
+from pathlib import Path
+
+BASE_URL = "https://example.com/"
+BASE_OUTPUT_PATH = Path("./output")
+SELENIUM_MAX_RETRIES = 5  # SELENIUMでエラーになった際に最大何回リトライするか
+SELENIUM_RETRY_DELAY = 5  # SELENIUMでエラーになった際に何秒待ってからリトライするか（単位は秒）
+SELENIUM_TIMEOUT = 30  # SELENIUMの最大待ち時間（単位は秒）
+SELENIUM_MAX_GET_COUNT = 20  # SELENIUMで何回getしたら再生成するか
+SCRAPING_TRANSITION_PAUSE = 1  # 各プロジェクトを取得する際の待ち時間
+# SCRAPING_END_PAGE = 200
+SCRAPING_END_PAGE = 2
